2a06a8e4af73ea7adf05c729fe682b22d12ec074
pupmiIk <joee.saavedra@gmail.com>
2023-09-05 22:18:54 +0800

    some colors and pics added

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,10 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        purple: "#781462",
+        purple: '#A484A9',
+        purpler: '#7B0E90',
+        purplerer: '#781462',
+        purplerest: '#92509C',
       },
     },
   },
