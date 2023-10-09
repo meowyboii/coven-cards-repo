021da15bdca6619c64b093ed0648462e9484d762
BuildTools <unconfigured@null.spigotmc.org>
2023-10-09 22:40:35 +0800

    commited

--- client/src/pages/User/Download.jsx
+++ client/src/pages/User/Download.jsx
@@ -24,7 +24,7 @@ export const Download = () => {
       </div>
       <div className="flex items-center justify-center w-[800px] h-[400px] bg-[#1E0523DF] p-10 relative rounded-3xl text-purple mt-[20vh] ml-[50vh] mr-[5vh] inline-block">
         <div className="card-header">
-          <h2 className = "text font-maintoo text-[43px]">DOWNLOAD & INSTALLATION</h2>
+          <h2 className = "text font-maintoo text-[40px]">DOWNLOAD & INSTALLATION</h2>
         </div>
         <ol className="list-decimal list-outside text-[20px] font-bold">
           <li>Click the download link for the Coven Cards Installer.</li>
