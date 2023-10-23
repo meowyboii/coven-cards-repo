5339f78a8ce19618af14a0c2c9eab0e620fcb71c
pupmiIk <joee.saavedra@gmail.com>
2023-10-23 11:30:33 +0800

    some admin

--- client/src/pages/Admin/AdminDashboard.jsx
+++ client/src/pages/Admin/AdminDashboard.jsx
@@ -1,13 +1,21 @@
 import React from "react";
+import bannerImg from "../../assets/img/home_playnow.png";
 import { AdminMenu } from "./AdminMenu";
 import { Layout } from "../../components/Layout";
 
 export const AdminDashboard = () => {
+  const container = {
+    backgroundImage: `url(${bannerImg})`,
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+  };
   return (
+    <section style={container} className="relative h-[100vh] p-10">
     <Layout>
       <div className="flex item-center justify-center py-[25vh]">
         <AdminMenu />
       </div>
     </Layout>
+    </section>
   );
 };
--- client/src/pages/Admin/AdminMenu.jsx
+++ client/src/pages/Admin/AdminMenu.jsx
@@ -4,36 +4,38 @@ import { Layout } from "../../components/Layout";
 
 export const AdminMenu = () => {
   return (
-    <div className="text-white p-4 w-1/6">
+    <div className="bg-[#1E0523bd] border-2 border-[#1E0523] relative rounded-3xl w-[70vh] h-[30vh] pt-10 text-purple flex justify-center">
+    <div className="text-white p-4 w-1/2">
       <div>
-        <h2 className="text-2xl font-bold mb-4 ">Admin Menu</h2>
+        <h2 className="text2 text-3xl font-bold mb-4 ">ADMIN MENU</h2>
         <ul className="space-y-2 ">
-          <li className="border-2">
+          <li className="border-2 rounded">
             <NavLink
               to={"/dashboard/admin/create-category"}
-              className="block hover:bg-gray-700 px-2 py-1 rounded active:bg-black"
+              className="block hover:bg-pink-900 px-2 py-1 rounded active:bg-black"
             >
               Create Category
             </NavLink>
           </li>
-          <li className="border-2">
+          <li className="border-2 rounded">
             <NavLink
               to={"/dashboard/admin/create-product"}
-              className="block hover:bg-gray-700 px-2 py-1 rounded active:bg-black"
+              className="block hover:bg-pink-900 px-2 py-1 rounded active:bg-black"
             >
               Create Product
             </NavLink>
           </li>
-          <li className="border-2">
+          <li className="border-2 rounded">
             <NavLink
               to={"/dashboard/admin/users"}
-              className="block hover:bg-gray-700 px-2 py-1 rounded active:bg-black"
+              className="block hover:bg-pink-900 px-2 py-1 rounded active:bg-black"
             >
               Manage Users
             </NavLink>
           </li>
         </ul>
       </div>
+    </div>
     </div>
   );
 };
