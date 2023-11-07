5581be80c857fd19ad157bad0b01b46bca5a6de8
Tim Midel <mideltim@gmail.com>
2023-11-07 22:38:04 +0800

    commit

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const { requireSignIn, isAdmin } = require("../middlewares/authMiddleware");
-const { getAllOrderController } = require("../controllers/orderController");
+const {
+  getAllOrderController,
+  updateOrderStatusController,
+} = require("../controllers/orderController");
 const router = express.Router();
 
 router.get("/get-order", requireSignIn, isAdmin, getAllOrderController);
-
+router.put(
+  "/update-order/:id",
+  requireSignIn,
+  isAdmin,
+  updateOrderStatusController
+);
 module.exports = router;
