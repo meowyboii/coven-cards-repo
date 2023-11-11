d4279e4b754287961de238fcfc15f680816dbf08
Tim Midel <mideltim@gmail.com>
2023-11-11 11:39:09 +0800

    commit

--- client/src/pages/User/Products.jsx
+++ client/src/pages/User/Products.jsx
@@ -6,6 +6,8 @@ import { Link } from "react-router-dom";
 export const Products = () => {
   const [products, setProducts] = useState([]);
   const [saleProducts, setSaleProducts] = useState([]);
+  const [newArrivals, setNewArrivals] = useState([]);
+  const currentDate = new Date();
 
   const getAllProducts = async (req, res) => {
     try {
@@ -27,6 +29,21 @@ export const Products = () => {
 
   useEffect(() => {
     setSaleProducts(products.filter((product) => product.sale === true));
+    setNewArrivals(
+      products.filter((product) => {
+        // Assuming product.createdAt is the property indicating the creation date
+        const createdAt = new Date(product.createdAt);
+
+        // Calculate the difference in milliseconds
+        const timeDifference = currentDate - createdAt;
+
+        // Calculate the difference in days
+        const daysDifference = timeDifference / (1000 * 60 * 60 * 24);
+
+        // Return true if the product was created within the last 7 days
+        return daysDifference < 7;
+      })
+    );
   }, [products]);
 
   return (
@@ -51,6 +68,35 @@ export const Products = () => {
                 <p className="text-lg text-red-200 mr-2">{`$${product.amountSale}`}</p>
                 <p className="ml-2 text-lg opacity-80 line-through">{`$${product.price}.00`}</p>
               </div>
+            </Link>
+          ))}
+        </div>
+      </div>
+      <div>
+        <h1 className="text-4xl font-bold p-4 text-[#f1e9f1] text-center">
+          NEW ARRIVALS
+        </h1>
+        <div className="grid grid-cols-4 gap-8 text-[#f1e9f1] px-40 pb-40 pt-10">
+          {newArrivals.map((product) => (
+            <Link key={product._id} to={`/merch/product/${product.slug}`}>
+              <div className="bg-gradient-to-br from-[#340449] to-[#0E0014] p-4 rounded shadow-md h-[39vh] mb-4 flex justify-center items-center">
+                <img
+                  src={`${process.env.REACT_APP_API}/api/v1/product/product-photo/${product._id}`}
+                  alt={product.name}
+                  className="w-full max-h-full object-cover transition-transform transform scale-100 hover:scale-105 transition-transform duration-500"
+                />
+              </div>
+              <h3 className="text-xl font-semibold">{product.name}</h3>
+              <>
+                {product.sale ? (
+                  <div className="flex content-center mt-2">
+                    <p className="text-lg text-red-200 mr-2">{`$${product.amountSale}`}</p>
+                    <p className="ml-2 text-lg opacity-80 line-through">{`$${product.price}.00`}</p>
+                  </div>
+                ) : (
+                  <p className="text-lg mt-2">{`$${product.price}.00`}</p>
+                )}
+              </>
             </Link>
           ))}
         </div>
